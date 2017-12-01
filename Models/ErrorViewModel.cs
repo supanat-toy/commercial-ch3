b8521f554a263cf89f7e09fe0ecf093a7ea3140f
ToyStory <supanat_toy@hotmail.com>
2017-12-01 22:18:56 +0700

    start project

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace commercial_ch3.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
